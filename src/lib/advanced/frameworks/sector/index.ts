6d7c5a3c3da2d9e6893b88774121d75360e0cfe9
dealintelligence0-cmd <dealintelligence0@gmail.com>
2026-05-01 22:14:40 +0530

    Create SectorFramework type and export modules
    Define SectorFramework type and export sector frameworks.

---
+++ src/lib/advanced/frameworks/sector/index.ts
@@ -0,0 +1,20 @@
+
+
+export type SectorFramework = {
+  analyticalQuestions: string[];
+  calculationHeuristics: string[];
+  industryBenchmarks: string[];
+  regulatoryWatchouts: string[];
+};
+
+export { pharmaSectorFramework } from "./pharma";
+export { consumerSectorFramework } from "./consumer";
+export { energySectorFramework } from "./energy";
+export { financialServicesSectorFramework } from "./financial_services";
+export { healthcareSectorFramework } from "./healthcare";
+export { industrialsSectorFramework } from "./industrials";
+export { lifeSciencesSectorFramework } from "./life_sciences";
+export { privateEquitySectorFramework } from "./private_equity";
+export { publicSectorFramework } from "./public_sector";
+export { realEstateSectorFramework } from "./real_estate";
+export { technologyMediaTelecomSectorFramework } from "./technology_media_telecom";
